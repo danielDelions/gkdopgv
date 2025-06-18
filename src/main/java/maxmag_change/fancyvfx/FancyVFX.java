38f72b3639e83251053a753927ed93557bcca80e
danielDelions <azulynho@gmail.com>
2025-06-17 21:16:07 -0300

    Add files via upload

---
+++ src/main/java/maxmag_change/fancyvfx/FancyVFX.java
@@ -0,0 +1,10 @@
+package maxmag_change.fancyvfx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FancyVFX implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("[FancyVFX] Mod inicializado com sucesso!");
+    }
+}
